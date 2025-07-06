a4e134aa6e63a074b033bdbdd4d28e4b72549c1a
rajani1111 <rajani.jogdand@gmail.com>
2025-07-07 01:40:17 +0530

    POJO class eclipse files are added to Rajani2 branch

---
+++ eclipse-workspace/POJOClassProject/src/main/java/package2/MessagePOJOClass.java
@@ -0,0 +1,67 @@
+package package2;
+
+public class MessagePOJOClass {
+
+	private String message;
+	private String greet;
+	
+	
+	public void setmessage(String message) {
+		this.message = message;
+	}
+	
+	public String getmessage() {
+		return message;
+	}
+	
+	
+	public void setgreet(String greet) {
+		this.greet=greet;
+	}
+	
+	public String getgreet() {
+		return greet;
+	}
+	
+
+	//public static void main(String[]args)
+	//
+
+
+//create java class
+
+//		MessagePOJOClass m= new MessagePOJOClass();
+//		m.setmessage("Hello");
+//		m.setgreet("Hi");
+//	
+//		m.getmessage();
+//		m.getgreet();
+//	//rest assured
+//	
+//	given()
+//	.body(m)
+//	.when()
+//	.post("/MessagePOJOClass");
+//}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*{
+ * "message":"Hello",
+ * "greet":"Hi"
+
+}*/
