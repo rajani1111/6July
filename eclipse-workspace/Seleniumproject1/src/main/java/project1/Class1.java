5a44a8ded3c8e61500cbfe6e7db06e74ba75db9c
rajani1111 <rajani.jogdand@gmail.com>
2025-07-07 01:44:13 +0530

    only Class1 is added in branch 3

---
+++ eclipse-workspace/Seleniumproject1/src/main/java/project1/Class1.java
@@ -0,0 +1,18 @@
+package project1;
+
+public class Class1 {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	
+	public class democlass
+	{
+		public String method1()
+		{
+		return "Rajani";	
+		}
+	
+	}
+}
